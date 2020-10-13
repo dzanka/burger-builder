8c6ba297ce843b9438a972ced864d1cf3735b7cb
dzanka <dadova.jana@gmail.com>
2020-10-13 23:26:58 +0200

    burger builder app until order button without order v.136

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import styles from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'Salad', type: 'salad'},
+    { label: 'Bacon', type: 'bacon'},
+    { label: 'Cheese', type: 'cheese'},
+    { label: 'Meat', type: 'meat'}
+]; //type musi sediet so state ingredients type
+
+const buildControls = (props) => (
+    <div className={styles.BuildControls}>
+        <p> Current Price: <strong>{props.price.toFixed(2)}e</strong></p>
+        { controls.map(ctrlKey => (
+            <BuildControl 
+                key={ctrlKey.label} 
+                label={ctrlKey.label} 
+                added = {() => props.ingredientAdded(ctrlKey.type)}
+                removed = {() => props.ingredientRemoved(ctrlKey.type)}
+                disabled= {props.disabled[ctrlKey.type]}
+            />
+        ))}
+        <button 
+            className={styles.OrderButton}
+            disabled = {!props.purchaseable}
+        >ORDER NOW</button>
+    </div>
+);
+
+export default buildControls;
---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import styles from './Burger.module.css';
+
+const burger = (props) => {
+    /*const transformedIngredients = Object.keys(props.ingredients).map(igkey => {
+        return [...Array(props.ingredients[igKey])].map(); //[,]
+    }); //z objektu pole
+    */
+
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />
+        }); //[,]
+    }).reduce((arr, el) => {
+        return arr.concat(el)
+    }, []); //Object. z objektu pole
+
+    if (transformedIngredients.length === 0){
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
+
+    return (
+        <div className={styles.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+};
+
+export default burger;
