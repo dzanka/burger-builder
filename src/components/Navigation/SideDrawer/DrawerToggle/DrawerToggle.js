74dc0cb9afc54a54da325166ce40cb35170b4811
dzanka <dadova.jana@gmail.com>
2020-10-18 10:44:42 +0200

    adds image to sideDrawer button v.151

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,11 +1,14 @@
 import React from 'react';
+
 import styles from './DrawerToggle.module.css';
 
 const drawerToggle = ( props ) => (
     <div 
         className={styles.DrawerToggle}
         onClick={props.clicked}>
-            MENU
+            <div></div>
+            <div></div>
+            <div></div>
     </div>
 );
  
